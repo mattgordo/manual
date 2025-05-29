8a07deed303e56e6407780ac746b4ca279ac8194
Matt <gordonster@gmail.com>
2025-05-29 16:59:38 +0100

    chore: add results page

--- components/Results/Results.tsx
+++ components/Results/Results.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 
+import Link from "next/link";
 import { hasPassedTest } from "./Results.helpers";
 
 import styles from './Results.module.scss';
@@ -9,9 +10,14 @@ import styles from './Results.module.scss';
 export const Results = () => {
   const hasPassed = hasPassedTest();
 
-  console.log('hasPassed', hasPassed);
+  const link = <Link href="https://www.manual.co">manual.co</Link>;
+
+  const passedText = <p className={styles.text}>Great news! We have the perfect treatment for your hair loss. Proceed to {link} and prepare to say hello to your new hair!</p>;
+  const rejectionText = <p className={styles.text}>Unfortunately, we are unable to prescribe this medication for you. This is because finasteride can alter the PSA levels, which may be used to monitor for cancer. You should discuss this further with your GP or specialist if you would still like this medication.</p>;
   
+  const result = hasPassed ? passedText : rejectionText;
+
   return <div className={styles.results}>
-    
+    {result}
   </div>
 }
