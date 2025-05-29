9fb93bc57784c33216fa850aec35960c1b8b6e26
Matt <gordonster@gmail.com>
2025-05-29 17:10:45 +0100

    chore: tidy up styles for quiz

--- components/Results/Results.tsx
+++ components/Results/Results.tsx
@@ -12,8 +12,16 @@ export const Results = () => {
 
   const link = <Link href="https://www.manual.co">manual.co</Link>;
 
-  const passedText = <p className={styles.text}>Great news! We have the perfect treatment for your hair loss. Proceed to {link} and prepare to say hello to your new hair!</p>;
-  const rejectionText = <p className={styles.text}>Unfortunately, we are unable to prescribe this medication for you. This is because finasteride can alter the PSA levels, which may be used to monitor for cancer. You should discuss this further with your GP or specialist if you would still like this medication.</p>;
+  const passedText = <div className={styles.text}>
+    <h3 className={styles.title}>Great news!</h3>
+    <p>We have the perfect treatment for your hair loss.</p>
+    <p>Proceed to {link} and prepare to say hello to your new hair!</p>
+  </div>;
+  const rejectionText = <div className={styles.text}>
+    <p>Unfortunately, we are unable to prescribe this medication for you.</p>
+    <p>This is because finasteride can alter the PSA levels, which may be used to monitor for cancer.</p>
+    <p>You should discuss this further with your GP or specialist if you would still like this medication.</p>
+  </div>;
   
   const result = hasPassed ? passedText : rejectionText;
 
